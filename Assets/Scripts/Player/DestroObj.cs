40492da15a8e92297310b8c49f2a70400fd883b7
yGabrielT <gtrindade380@gmail.com>
2023-06-20 19:06:58 -0300

    finalizada a mecanica de explosão da granada

--- Assets/Scripts/Player/DestroObj.cs
+++ Assets/Scripts/Player/DestroObj.cs
@@ -5,16 +5,18 @@ using UnityEngine;
 public class DestroObj : MonoBehaviour
 {
     public GameObject ExploHitFX;
+    [SerializeField]private float raio = 10f;
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag != ("Inimigo") && other.gameObject.tag != ("Player") )
         {
             if (this.gameObject.tag == ("Bombs"))
             {
+                Explode();
                 Instantiate(ExploHitFX, transform.position, Quaternion.identity);
 
             }
-            Destroy(gameObject);
+            Destroy(this.gameObject);
             
         }
 
@@ -22,14 +24,38 @@ public class DestroObj : MonoBehaviour
         {
             if(this.gameObject.tag == ("Bombs"))
             {
+                Explode();
                 Instantiate(ExploHitFX, transform.position, Quaternion.identity);
+                Destroy(this.gameObject);
             }
-            
-            other.GetComponent<Eliminar>().TomarDano(10);
-            Destroy(gameObject);
-            Debug.Log("Atingido");
-            
+            else
+            {
+                other.GetComponent<Eliminar>().TomarDano(10);
+                Destroy(gameObject);
+                Debug.Log("Atingido");
+            }
         }
         
+    }
+
+    private void Explode()
+    {
+        Collider[] colisores = Physics.OverlapSphere(transform.position, raio);
+        Destroy(gameObject);
+        foreach (Collider nearbyObjs in colisores)
+        {
+            if (nearbyObjs != null && nearbyObjs.gameObject.tag == ("Inimigo"))
+            {
+                nearbyObjs.GetComponent<Eliminar>().TomarDano(100);
+                Debug.Log("Raio Atingido");
+            }
+
+
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(transform.position, raio);
     }
 }
