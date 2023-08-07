f9c24163ca5716825441aa16528f4275e8294493
yGabrielT <gtrindade380@gmail.com>
2023-08-07 10:35:23 -0300

    Script q deu erro em merge

--- Assets/Scripts/Player/Personagem.cs
+++ Assets/Scripts/Player/Personagem.cs
@@ -23,11 +23,8 @@ public class Personagem : MonoBehaviour
     [SerializeField] private float normalSensivity = 1f;
     [SerializeField] private float aimSensivity = .5f;
     [SerializeField] private LayerMask _mask;
-    [SerializeField] private GameObject _crosshair;
-    [SerializeField] private ParticleSystem SmokeTrail;
-    [Range(0f,0.2f)]
-    [SerializeField] private float tempoFormacaoPoeira;
     [SerializeField] private float ShakeForce = 0.1f;
+    [SerializeField] private GameObject _crosshair;
     public RaycastHit raycastHit;
     public bool isLooking;
     public AudioSource audioMorte;
@@ -41,6 +38,20 @@ public class Personagem : MonoBehaviour
     private ThirdPersonController _controller;
     private StarterAssetsInputs _input;
     public Inimigo inimigo;
+
+    [Header("VFX")]
+    [SerializeField] private ParticleSystem SmokeWalkTrail;
+    [SerializeField] private ParticleSystem SmokeSprintTrail;
+    [SerializeField] private GameObject SmokeJump;
+    [SerializeField] private GameObject SmokeFall;
+    [Range(0f, 2f)]
+    [SerializeField] private float tempoFormacaoPoeiraWalk;
+    [Range(0f, 2f)]
+    [SerializeField] private float tempoFormacaoPoeiraSprint;
+    [SerializeField] private Transform vfxPos;
+    private bool isInAir = false;
+    private float contadorWalk;
+    private float contadorSprint;
     // Start is called before the first frame update
     private void Awake()
     {
@@ -148,14 +159,37 @@ public class Personagem : MonoBehaviour
 
     private void SmokeUpdate()
     {
-        contador += Time.deltaTime;
-        if(SmokeTrail != null && _input.sprint && _controller._controller.isGrounded)
+        //Rastro para quando estiver andando
+        contadorWalk += Time.deltaTime;
+        if (SmokeWalkTrail != null && !_input.sprint && _controller._controller.isGrounded && _controller._controller.velocity != Vector3.zero) 
         {
-            if(contador > tempoFormacaoPoeira)
+            if (contadorWalk > tempoFormacaoPoeiraWalk)
             {
-                SmokeTrail.Play();
-                contador = 0;
+                SmokeWalkTrail.Play();
+                contadorWalk = 0;
             }
+                
+        }
+        //Rastro pra correr
+        contadorSprint += Time.deltaTime;
+        if (SmokeSprintTrail != null && _input.sprint && _controller._controller.isGrounded && _controller._controller.velocity != Vector3.zero)
+        {
+            if (contadorSprint > tempoFormacaoPoeiraSprint)
+            {
+                SmokeSprintTrail.Play();
+                contadorSprint = 0;
+            }
+        }
+        //Rastro pra pular
+        if (SmokeJump != null && _input.jump && !_controller._controller.isGrounded && !isInAir)
+        {
+            isInAir = true;
+            Instantiate(SmokeJump, vfxPos.position, Quaternion.identity);
+        }
+        if (SmokeFall != null && _controller._controller.isGrounded && isInAir && !_input.jump)
+        {
+            isInAir = false;
+            Instantiate(SmokeFall, vfxPos.position, Quaternion.identity);
         }
     }
 
