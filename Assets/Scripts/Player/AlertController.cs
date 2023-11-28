b2b92954572e2778eef51da7d7a4209548925a1d
yGabrielT <gtrindade380@gmail.com>
2023-11-28 16:25:44 -0300

    Versão 0.6.8
    -Adicionado dialogos para os própios pensamentos.
    -Adicionado sons pra cutscene do boss e holograma
    -Adicionado algumas notificações

--- Assets/Scripts/Player/AlertController.cs
+++ Assets/Scripts/Player/AlertController.cs
@@ -5,25 +5,27 @@ using TMPro;
 
 public class AlertController : MonoBehaviour
 {
-    [SerializeField] private Collider colisao;
+    
     [SerializeField] private GameObject alertBox;
     [SerializeField] private Animator alertaAnimator;
     public string textoAplicado; 
     [SerializeField] private TMP_Text alertaTxt;
 
     // Update is called once per frame
-    void Update()
-    {
-
+    void Start(){
+        
     }
-
     private void OnTriggerEnter(Collider colisaoInstrucao)
     {
+        Notificacao();
+        
+    }
+
+    public void Notificacao(){
         alertBox.SetActive(false);
         alertBox.SetActive(true);
-        Destroy(colisao);
+        
         alertaAnimator.Play("alerta");
         alertaTxt.text = textoAplicado;
-        
     }
 }
